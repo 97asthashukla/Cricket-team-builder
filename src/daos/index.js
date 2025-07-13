b871d3c551a9f1a30141c4aaee932402dfc2867c
97asthashukla <97asthashukla@gmail.com>
2025-07-13 11:52:57 +0530

    General Service/DAO Index Files

---
+++ src/daos/index.js
@@ -0,0 +1,9 @@
+const userDao = require('./userDao');
+const playerDao = require('./playerDao');
+const teamDao = require('./teamDao');
+
+module.exports = {
+  userDao,
+  playerDao,
+  teamDao,
+};
---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+const authService = require('./authService');
+const playerService = require('./playerService');
+const teamService = require('./teamService');
+const teamValidation = require('./teamValidation');
+
+module.exports = {
+  authService,
+  playerService,
+  teamService,
+  teamValidation,
+};
